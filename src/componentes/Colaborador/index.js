f1715740afb8d9151c7c20ad5a177f4a5281b2a1
Kevin Lopes Paschoal <kevinlp@live.com>
2023-10-31 21:04:52 -0300

    Criado card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css"
+
+const Colaborador = (props) =>{
+
+   return (<div className="colaborador">
+      <div className="cabecalho">
+         <img src="https://github.com/kevinlpll.png" alt="Kevin Lopes Paschoal"/>
+      </div>
+      <div className="rodape">
+         <h4>Kevin Lopes</h4>
+         <h5>Developes</h5>
+      </div>
+   </div>)
+
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,17 +1,22 @@
+import Colaborador from "../Colaborador"
 import "./Time.css"
+
 
 const Time = (props) => {
 
    const estilos = {
-      time: { backgroundColor: props.corSecundaria}
+      time: { backgroundColor: props.corSecundaria},
    }
 
    return (
       <section className="time" style={ estilos.time } >
          <h3 style={{ borderColor: props.corPrimaria }}>{ props.nome }</h3>
+         <div>
+            <Colaborador />
+            <Colaborador />
+         </div>
       </section>
    )
-
    
 }
 
